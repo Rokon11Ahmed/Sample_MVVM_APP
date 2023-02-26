5a22693be1c225e1ac83ece84dff3f98bf0b5304
Rokon Ahmed <rokon11ahmed@gmail.com>
2023-02-26 15:30:30 +0600

    implement sandwitch for network adapter factory

--- domain/src/main/java/com/example/domain/login/GetLoginUseCase.kt
+++ domain/src/main/java/com/example/domain/login/GetLoginUseCase.kt
@@ -1,31 +1,21 @@
 package com.example.domain.login
 
-import android.util.Log
 import com.example.domain.common.Resource
 import com.example.domain.model.UserInfo
 import com.example.domain.repository.LoginRepository
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
-import java.io.IOException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetLoginUseCase @Inject constructor(
     private val repository: LoginRepository
 ) {
-    operator fun invoke(userName: String, pin: String): Flow<Resource<UserInfo>> = flow {
-        emit(Resource.Loading())
-        try {
-            Log.d("TAG", "invoke called: ")
-
-            repository.getLoginResponse(name = userName, pin = pin)
-                .onEach {dataState ->
-                    emit(dataState)
-                }.flowOn(Dispatchers.Main)
-        } catch (e: IOException) {
-            emit(Resource.Error("Couldn't reach server. Check your internet connection!"))
-        }
-    }
+    operator fun invoke(userName: String, pin: String): Flow<Resource<UserInfo>> =
+        repository.getLoginResponse(name = userName, pin = pin)
+            .map {
+                return@map it
+            }.flowOn(Dispatchers.IO)
 
 }
